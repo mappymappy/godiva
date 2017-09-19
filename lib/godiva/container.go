328584e95c9c1fda380a2f89dd1cb2cdcc9ae0ee
marnie_ms4 <mappymappy0301@i.softbank.jp>
2017-09-19 16:32:54 +0900

    add lock

--- lib/godiva/container.go
+++ lib/godiva/container.go
@@ -3,13 +3,17 @@ package godiva
 
 import (
 	"fmt"
+	"sync"
 )
+
+var mutex sync.Mutex
+var cachem sync.Mutex
 
 // Container
 type Container struct {
 
 	// Anonymous functions for object factory
-	factories    map[string]func(c *Container) (interface{}, error)
+	factories map[string]func(c *Container) (interface{}, error)
 
 	// Created Object Cache
 	cacheStorage map[string]interface{}
@@ -27,16 +31,16 @@ func CreateContainer() *Container {
 
 // Register FactoryFunction
 func (c *Container) Register(key string, factory factoryFunction) {
+	mutex.Lock()
+	defer mutex.Unlock()
 	c.factories[key] = factory
 }
 
 // Create Object By Key
 func (c *Container) Create(key string, ignoreCache bool) (interface{}, error) {
-
 	if _, exist := c.factories[key]; !exist {
 		return nil, fmt.Errorf("key:%s called unregistered resource", key)
 	}
-
 	if c.cacheStorage[key] != nil && !ignoreCache {
 		return c.cacheStorage[key], nil
 	}
@@ -46,7 +50,9 @@ func (c *Container) Create(key string, ignoreCache bool) (interface{}, error) {
 	if err != nil {
 		return nil, err
 	}
+	cachem.Lock()
 	c.cacheStorage[key] = Object
+	cachem.Unlock()
 
 	return Object, nil
 }
